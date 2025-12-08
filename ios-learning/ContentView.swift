9c0fa00e4e60f6e8d0272f761993f1d687a16753
彭少林 <170341224@cauc.edu.cn>
2025-12-08 20:15:10 +0800

    AnimatedTabView

--- ios-learning/ContentView.swift
+++ ios-learning/ContentView.swift
@@ -8,10 +8,79 @@
 import SwiftUI
 import SwiftData
 
+/// tab items
+enum AppTab: AnimatedTabSelectionProtocol{
+    case call
+    case notifications
+    case settings
+    
+    var symbolImage: String{
+        switch self{
+        case .call: return "phone.down.waves.left.and.right"
+        case .notifications :return "bell.badge"
+        case .settings: return "gearshape.fill"
+        }
+    }
+    
+    var title: String{
+        switch self{
+        case .call: return "Call"
+        case .notifications :return "Notifications"
+        case .settings: return "Settings"
+        }
+    }
+}
+
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    
+    @State private var selectTab: AppTab = .call
 
+    var body: some View {
+        AnimatedTabView(selection: $selectTab){
+            /// you can use the Native Tab just like normal Tabview
+            Tab.init(AppTab.call.title, systemImage: AppTab.call.symbolImage, value: .call){
+                ItemListView(items: items, addItem: addItem, deleteItems: deleteItems)
+            }
+            
+            Tab.init(AppTab.notifications.title, systemImage: AppTab.notifications.symbolImage, value: .notifications){
+                Text("Notifications")
+            }
+            
+            Tab.init(AppTab.settings.title, systemImage: AppTab.settings.symbolImage, value: .settings){
+                Text("Settings")
+            }
+        }effects: { tab in
+            switch tab {
+            case .call : [.bounce.up]
+            case .notifications : [.wiggle]
+            case .settings : [.rotate]
+            }
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+struct ItemListView: View {
+    let items: [Item]
+    let addItem: () -> Void
+    let deleteItems: (IndexSet) -> Void
+    
     var body: some View {
         NavigationSplitView {
             List {
@@ -41,21 +110,6 @@ struct ContentView: View {
             }
         } detail: {
             Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
         }
     }
 }
---
+++ ios-learning/Helpers/AnimatedTabView.swift
@@ -0,0 +1,105 @@
+//
+//  AnimatedTabView.swift
+//  ios-learning
+//
+//  Created by 彭少林 on 2025/12/8.
+//
+
+import SwiftUI
+
+/// Selection protocol
+protocol AnimatedTabSelectionProtocol: CaseIterable, Hashable{
+    var title: String { get }
+    var symbolImage: String { get }
+}
+
+struct AnimatedTabView<Selection: AnimatedTabSelectionProtocol, Content: TabContent<Selection>>: View {
+    @Binding var selection: Selection
+    @TabContentBuilder<Selection> var contet : () -> Content
+    
+    /// View Properties
+    @State private var imageViews: [Selection: UIImageView] = [:]
+    
+    var effects: (Selection) -> [any DiscreteSymbolEffect & SymbolEffect]
+    
+    var body: some View{
+        TabView(selection: $selection){
+            contet()
+        }
+        .tabViewStyle(.tabBarOnly)
+        .background(ExtractImageViewsFromTabView{
+            imageViews = $0
+        })
+        .compositingGroup()
+        .onChange(of: selection) { oldValue, newValue in
+            let symbolEffects = effects(newValue)
+            guard let imageView = imageViews[newValue] else { return }
+            
+            for effect in symbolEffects {
+                imageView.addSymbolEffect(effect, options: .nonRepeating)
+            }
+        }
+    }
+}
+
+fileprivate struct ExtractImageViewsFromTabView<Value: AnimatedTabSelectionProtocol>: UIViewRepresentable{
+    var result: ([Value: UIImageView]) -> ()
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.isUserInteractionEnabled = false
+        DispatchQueue.main.async {
+            if let compostingGroup = view.superview?.superview{
+                guard let tabHostingController = compostingGroup.subviews.last else {return}
+                guard let tabController = tabHostingController.subviews.first?.next as?
+                        UITabBarController else {return}
+
+                extractImageViews(tabController.tabBar)
+            }
+        }
+        return view
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    private func extractImageViews(_ tabBar: UITabBar){
+        let imageViews = tabBar.subviews(type: UIImageView.self)
+        /// filter out on symbol Images
+            .filter({$0.image?.isSymbolImage ?? false})
+            .filter({isIOS26 ? ($0.tintColor == tabBar.tintColor) : true})
+        
+        
+        var dict: [Value: UIImageView] = [:]
+        for tab in Value.allCases{
+            if let imageView = imageViews.first(where: {
+                $0.description.contains(tab.symbolImage)
+            }){
+                dict[tab] = imageView
+                
+            }
+        }
+        result(dict)
+    }
+    
+    private var isIOS26: Bool{
+        if #available(iOS 26, *){
+            return true
+        }
+        return false
+    }
+    
+}
+
+/// Extracting All subviews with the given type!
+fileprivate extension UIView{
+    func subviews<T: UIView>(type: T.Type) -> [T]{
+        subviews.compactMap {$0 as? T} +
+        subviews.flatMap { $0.subviews(type: type)}
+    }
+}
+
+
+//#Preview {
+//    AnimatedTabView()
+//}
